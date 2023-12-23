ca11081e2614202c9b80438d789f069bc4e88100
sabinagf <142227408+sabinagf@users.noreply.github.com>
2023-12-23 17:05:26 -0500

    added files

---
+++ GoGreenProject/backend.tf
@@ -0,0 +1,10 @@
+# backend.tf
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket         = "gogreen.bucket"
+    key            = "gogreenproject"
+    region         = "us-east-2"  # Replace with your desired AWS region
+   profile = "terraform-user"
+  }
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,119 @@
+
+
+#create vpc
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+enable_dns_hostnames = true
+  tags = {
+    Name = "${var.project_name}"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+vpc_id = aws_vpc.vpc.id
+ tags = {
+    Name = "${var.project_name}-igw"
+    } 
+}
+
+
+#use data source to get all avalability zones in region
+data "aws_availability_zones" "availability_zones" {}
+
+
+# create a public subnet az1
+
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_az1_cidr
+availability_zone = data.aws_availability_zones.availability_zones.names [0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public subnet az1"
+  }
+}
+
+#create public subnet az2
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_az2_cidr
+availability_zone = data.aws_availability_zones.availability_zones.names [1]
+map_public_ip_on_launch = true
+  tags = {
+    Name = "public subnet az2 "
+  }
+}
+
+# create route table add public route
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "public route table"
+  }
+}
+
+#associate public subnet az1 to "public route table"
+resource "aws_route_table_association" "public_subent_az1_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+# associate public subnet az2 to "public route table"
+resource "aws_route_table_association" "public_subent_az2_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+#create private subnet az1
+resource "aws_subnet" "private_app_subnet_az1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_app_subnet_az1_cidr
+  availability_zone = data.aws_availability_zones.availability_zones.names [0]
+map_public_ip_on_launch = false
+  tags = {
+    Name = "private app subnet az1"
+  }
+}
+
+
+#creare private app subnet az2
+resource "aws_subnet" "private_app_subnet_az2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_app_subnet_az2_cidr
+  availability_zone = data.aws_availability_zones.availability_zones.names [1]
+map_public_ip_on_launch = false
+  tags = {
+    Name = "private app subnet az2"
+  }
+}
+
+
+#create private data subnet az1
+resource "aws_subnet" "private_data_subnet_az1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_data_subnet_az1_cidr
+  availability_zone = data.aws_availability_zones.availability_zones.names [0]
+map_public_ip_on_launch = false
+  tags = {
+    Name = "private data subnet az1"
+  }
+}
+
+#create private data subnet az2
+resource "aws_subnet" "private_data_subnet_az2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_data_subnet_az2_cidr
+  availability_zone = data.aws_availability_zones.availability_zones.names [1]
+map_public_ip_on_launch = false
+  tags = {
+    Name = "private data subnet az2"
+  }
+}
