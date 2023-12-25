646581867512c3fbca59952ea11f7b2d344fd270
sabinagf <142227408+sabinagf@users.noreply.github.com>
2023-12-24 19:47:50 -0500

    updated

--- GoGreenProject/iam.tf
+++ GoGreenProject/iam.tf
@@ -59,92 +59,87 @@ resource "aws_iam_user_group_membership" "monitor_group_membership" {
 
 
 
-# #GIVING PERMISSIONS FOR USERS
+#GIVING PERMISSIONS FOR USERS
 
-# # Attach the AdministratorAccess policy to each user
-# resource "aws_iam_user_policy_attachment" "sysadmin_admin_policy_attachment" {
-#   for_each = toset(var.sysadmin_usernames)
+# Attach the AdministratorAccess policy to each user
+resource "aws_iam_user_policy_attachment" "sysadmin_admin_policy_attachment" {
+  for_each = var.sysadmin_users
 
-#   user       = aws_iam_user.sysadmin_users[each.key].name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
+  user       = aws_iam_user.sysadmin_users[each.key].name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
 
-# #Attach the AmazonRDSFUllAccess policy to each user
+#Attach the AmazonRDSFUllAccess policy to each user
 
-# resource "aws_iam_user_policy_attachment" "dbadmin_rds_policy_attachment" {
-# for_each = toset(var.dbadmin_usernames)
+resource "aws_iam_user_policy_attachment" "dbadmin_rds_policy_attachment" {
+for_each = var.dbadmin_users
 
-# user = aws_iam_user.dbadmin_users[each.value].name
-# policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-# }
+user = aws_iam_user.dbadmin_users[each.key].name
+policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
 
-# # #Attach the AmazonS3ReadOnlyAccess, Amazon RDSReadonlyAccess, AmazonEC2ReadOnlyAccess to each user
-# resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_s3" {
-#   for_each = toset(var.monitor_user_names)
+# #Attach the AmazonS3ReadOnlyAccess, Amazon RDSReadonlyAccess, AmazonEC2ReadOnlyAccess to each user
+resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_s3" {
+  for_each = var.monitor_users
 
-#   user      = aws_iam_user.monitor_users[each.key].name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-# }
+  user      = aws_iam_user.monitor_users[each.key].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
 
-# resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_rds" {
-#   for_each = toset(var.monitor_user_names)
+resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_rds" {
+  for_each =var.monitor_users
 
-#   user      = aws_iam_user.monitor_users[each.key].name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
-# }
+  user      = aws_iam_user.monitor_users[each.key].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
+}
 
-# resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_ec2" {
-#   for_each = toset(var.monitor_user_names)
-
-#   user      = aws_iam_user.monitor_users[each.key].name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-# }
-
+resource "aws_iam_user_policy_attachment" "monitor_policy_attachment_ec2" {
+  for_each =var.monitor_users
+  user      = aws_iam_user.monitor_users[each.key].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
 
 
+ #create IAM role:EC2toS3IAMRole
+ resource "aws_iam_role" "ec2_to_s3_iam_role" {
+  name = "EC2toS3IAMRole"
 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
+resource "aws_iam_policy" "s3_read_only_policy" {
+  name        = "S3ReadOnlyPolicy"
+  description = "Policy for read-only access to Amazon S3"
 
-#  #create IAM role:EC2toS3IAMRole
-#  resource "aws_iam_role" "ec2_to_s3_iam_role" {
-#   name = "EC2toS3IAMRole"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:Get*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "ec2.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# resource "aws_iam_policy" "s3_read_only_policy" {
-#   name        = "S3ReadOnlyPolicy"
-#   description = "Policy for read-only access to Amazon S3"
-
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "s3:Get*",
-#       "Resource": "*"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# resource "aws_iam_role_policy_attachment" "ec2_to_s3_policy_attachment" {
-#   role       = aws_iam_role.ec2_to_s3_iam_role.name
-#   policy_arn = aws_iam_policy.s3_read_only_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "ec2_to_s3_policy_attachment" {
+  role       = aws_iam_role.ec2_to_s3_iam_role.name
+  policy_arn = aws_iam_policy.s3_read_only_policy.arn
+}
 
